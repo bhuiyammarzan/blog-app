7b9720673e88b5ee476ba7fef993d3f07903492d
bhuiyanmarzan <samimbuian12@gmail.com>
2026-01-13 10:50:43 +0600

    remove unused middleware and request import from Kernel and api routes

--- app/Http/Kernel.php
+++
@@ -1,9 +0,0 @@
-'api' => [
-    \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-    'throttle:api',
-    \Illuminate\Routing\Middleware\SubstituteBindings::class,
-],
-
-protected $routeMiddleware = [
-    'valid.token' => \App\Http\Middleware\EnsureValidToken::class,
-];
